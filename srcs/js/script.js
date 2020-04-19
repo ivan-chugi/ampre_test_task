e2fb8d2b834c28849b8a2a90613700e5ad6c4119
wcspoiler <wcspoiler@yandex.ru>
2020-04-19 15:56:10 +0300

    Tablet version

--- srcs/js/script.js
+++ srcs/js/script.js
@@ -1,27 +1,72 @@
-   let swiper = new Swiper('.swiper-container', {
-      effect: 'coverflow',
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: 'auto',
-      coverflowEffect: {
-        rotate: 0,
-        stretch: -32,
-        depth: 140,
-        modifier: 1,
-        slideShadows : true,
-      },
-      pagination: {
-        el: '.swiper-pagination',
-      },
+// if (window.innerWidth < 1024) {
+//     let swiper = new Swiper('.swiper-container', {
+//     effect: 'coverflow',
+//     grabCursor: true,
+//     centeredSlides: true,
+//     slidesPerView: 'auto',
+//     coverflowEffect: {
+//         rotate: 0,
+//         stretch: -32,
+//         depth: 140,
+//         modifier: 1,
+//         slideShadows : true,
+//     },
+//     // pagination: {
+//     //   el: '.swiper-pagination',
+//     // },
+//     });
+// }
+
+const breakpoint = window.matchMedia( '(min-width:64em)' );
+let mySwiper;
+
+const breakpointChecker = function() {
+    if ( breakpoint.matches === true ) {
+       if ( mySwiper !== undefined )
+         mySwiper.destroy( true, true );
+       return;
+    } else if ( breakpoint.matches === false ) {
+       return enableSwiper();
+    }
+ };
+
+ const enableSwiper = function() {
+    mySwiper = new Swiper ('.swiper-container', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+            rotate: 0,
+            stretch: -32,
+            depth: 140,
+            modifier: 1,
+            slideShadows : true,
+        }
     });
+ };
+ breakpoint.addListener(breakpointChecker);
+ breakpointChecker();
+
 
 let burger = document.querySelector(".toggle-nav__checkbox");
 let menu = document.querySelector(".nav-main");
+let user_list = document.querySelector(".user-list");
+let delay = 600;
 
 burger.addEventListener("change", function() {
-    if (this.checked && (window.innerWidth < 1024) ) {
+    if (this.checked && (window.innerWidth < 1024)) {
         menu.style.top = '0';
     } else if (!this.checked && (window.innerWidth < 1024)){
-        menu.style.top = '-100%';
+        menu.style.top = '-127%';
+    } else if (this.checked && (window.innerWidth > 1024 && window.innerWidth < 1440)) {
+        user_list.style.opacity = "1";
+        user_list.style.visibility = "visible";
+
+    } else if (!this.checked && (window.innerWidth > 1024 && window.innerWidth < 1440)) {
+        user_list.style.opacity = "0";
+        setTimeout(function() {
+            user_list.style.visibility = "hidden";
+          }, delay);
     }
 });
