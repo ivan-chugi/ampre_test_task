e672e6c486a9c6c24590d0096caad4dbbf932917
ivan-chugi <wcspoiler@yandex.ru>
2020-04-20 08:55:43 +0300

    Added gulp automation functionality

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browser_sync = require('browser-sync');
+let gulp = require('gulp');
+let plumber = require('gulp-plumber');
+let minifyCSS = require('gulp-minify-css');
+let sourcemap = require('gulp-sourcemaps');
+let sass = require('gulp-sass');
+let postcss = require('gulp-postcss');
+let autoprefixer = require('autoprefixer');
+let browser_sync = require('browser-sync');
+
 gulp.task('sass', function(){
     return gulp.src('srcs/scss/style.scss')
+    .pipe(plumber())
+    .pipe(sourcemap.init())
     .pipe(sass())
+    .pipe(postcss([
+        autoprefixer()
+    ]))
+    .pipe(minifyCSS())
+    .pipe(sourcemap.write("."))
     .pipe(gulp.dest('srcs/css'))
     .pipe(browser_sync.stream());
 });
+
 gulp.task('start', function() {
     browser_sync.init({
         server: {
